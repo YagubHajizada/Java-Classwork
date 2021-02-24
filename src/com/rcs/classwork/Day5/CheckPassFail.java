a055a68e62e1a7fdddf70af221f0e1b5086d7738
YagubHajizada <yagubhajizada9@gmail.com>
2021-02-24 20:19:06 +0200

    .

--- src/com/rcs/classwork/Day5/CheckPassFail.java
+++ src/com/rcs/classwork/Day5/CheckPassFail.java
@@ -6,13 +6,14 @@ public class CheckPassFail {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("ievadiet x :");
         int x = scanner.nextInt();
         scanner.close();
         if (x >= 50) {
             System.out.println("PASS");
         } if (x < 50) {
             System.out.println("FAIL");
-        } else if (!(x >= 50 && x < 50)) {
+        } else {
             System.out.println("DONE");
                  }
             }
