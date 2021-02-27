3d04f088b6b915f0018f965721df675793246ddb
YagubHajizada <yagubhajizada9@gmail.com>
2021-02-27 11:24:34 +0200

    .

--- src/com/rcs/classwork/Day6/IF.java
+++ src/com/rcs/classwork/Day6/IF.java
@@ -8,9 +8,9 @@ public class IF {
         System.out.println("ievadiet x :");
         int x = scanner.nextInt();
         scanner.close();
-        if (x % 2 == 0 && x <= 20 && x >= 6) {
+        if (x % 2 != 0 || (x <= 20 && x >= 6)) {
             System.out.println("Divaini");
-        } else if (x % 2 == 0 && x <= 5 && x >= 2 && x >20) {
+        } else {
             System.out.println("Nav divaini");
         }
     }
