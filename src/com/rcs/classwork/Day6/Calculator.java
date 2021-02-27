23884680dc672b7c357977820387e111474a5870
YagubHajizada <yagubhajizada9@gmail.com>
2021-02-27 12:25:20 +0200

    .

---
+++ src/com/rcs/classwork/Day6/Calculator.java
@@ -0,0 +1,33 @@
+package com.rcs.classwork.Day6;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ievadiet a :");
+        float a = scanner.nextFloat();
+        System.out.print("ievadiet b:");
+        float b = scanner.nextFloat();
+        scanner.nextLine();
+
+        System.out.print("Ievadiet darbibu (+,-,*,/): ");
+        String action = scanner.nextLine();
+        scanner.close();
+        switch (action) {
+            case "+":
+                System.out.println(a + b);
+                break;
+            case "-":
+                System.out.println(a - b);
+                break;
+            case "/":
+                System.out.println(a / b);
+                break;
+            case "*":
+                System.out.println(a * b);
+            default:
+                System.out.println("END");
+        }
+    }
+}
